de6edd17b7259a203e15fcb92f4f3762eec4f55a
fbrcosta <fbrcosta@d9f18e8a-2368-9922-8e25-0f7efa564f5d>
2011-04-21 14:32:30 +0000

    + cenas e metade do servidor feito

--- trunk/UPD++/src/Servidor/Connection.java
+++ trunk/UPD++/src/Servidor/Connection.java
@@ -5,21 +5,38 @@ import java.net.InetAddress;
 
 public class Connection{
 
+    private static int indice;
     private InetAddress ipAddress;
     private DatagramSocket socket;
 
-    private Reciever reciever;
-    private Sender sender;
+    private static Reciever reciever;
+    private static Sender sender;
 
 
-    Connection(InetAddress IPAddress, DatagramSocket socket){
+    Connection(int i,InetAddress IPAddress, DatagramSocket socket){
+        indice = i;
         this.ipAddress=IPAddress;
         this.socket=socket;
         reciever=new Reciever(socket);
         sender= new Sender(socket);
     }
 
+    public static void main(){
+        try {
+            reciever.start();
+            sender.start();
 
+            reciever.join();
+            sender.join();
 
+            ConnectionAccepter.eliminaConnection(indice);
+        } catch (InterruptedException ex) {
+            System.out.println("ERRO (Connection.main): " + ex.getMessage());
+        }
+    }
+
+    public int getIndice(){
+        return indice;
+    }
 
 }
