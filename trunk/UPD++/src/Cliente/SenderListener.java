d4aefd30f661dc3b3738f355800bf95e5591afa0
jbteixeir@gmail.com <jbteixeir@gmail.com@d9f18e8a-2368-9922-8e25-0f7efa564f5d>
2011-05-17 23:44:29 +0000

    TimeCounter(done) falta testar

--- trunk/UPD++/src/Cliente/SenderListener.java
+++ trunk/UPD++/src/Cliente/SenderListener.java
@@ -16,5 +16,6 @@ public interface SenderListener extends EventListener{
     public void pacotesGerados(SenderEvent e);
     public void pacotesEnviados(SenderEvent e);
     public void pacoteEnviado(SenderEvent e);
+    //public void mudouTamanhoJanela(SenderEvent e);
 
 }
