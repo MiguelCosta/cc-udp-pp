db50d446c5b3dcf7e0e4c83222be4b9de74b860b
jbteixeir@gmail.com <jbteixeir@gmail.com@d9f18e8a-2368-9922-8e25-0f7efa564f5d>
2011-05-17 21:26:07 +0000

    TimeCounter(half done)

--- trunk/UPD++/src/Cliente/TimeCounter/TimeCounter.java
+++ trunk/UPD++/src/Cliente/TimeCounter/TimeCounter.java
@@ -1,33 +1,83 @@
 package Cliente.TimeCounter;
 
+import Cliente.MainCliente;
 import java.util.ArrayList;
 
 public class TimeCounter extends Thread{
     
-    ArrayList<Long> timeOut;
-    long estimatedRTT;
-    long sampleRTT;
-    int alpha;
+    public ArrayList<Long> timeCountList;
+    private long timeout;
+    private long estimatedRTT;
+    private long sampleRTT;
+    private long devRTT;
+    private float alpha, beta;
+    private boolean keeprunning;
     
-    TimeCounter(ArrayList<Long> timeOut, Long estimatedRTT, int alpha){
-        this.timeOut=timeOut;
+    /*
+     * Construtor
+     * Recebe array de tempos dos pacotes, o RTT
+     */
+    TimeCounter(ArrayList<Long> timeCountList, Long estimatedRTT){
+        this.timeCountList=timeCountList;
         this.estimatedRTT=estimatedRTT;
-        this.alpha=alpha;
+        this.alpha=(float) 0.125;
+        this.beta=(float) 0.25;
+        this.keeprunning=true;
+        
     }
     
+    /*
+     * Parar a Thread
+     */
+    public void stoprunning(){
+        keeprunning=false;
+    }
+    
+    /*
+     * Trata a chegada de um novo Ack
+     */
     public void newAck(int index){
-        sampleRTT= System.currentTimeMillis() - timeOut.get(index);
-        timeOut.set(index, Long.valueOf(0));
+        sampleRTT= System.currentTimeMillis() - timeCountList.get(index);
+        timeCountList.set(index, Long.valueOf(0));
         this.estimateRTT();
+        this.calculateDevRTT();
+        this.calculateTimeOut();
     }
     
+    /*
+     * Estima o RTT baseando-se no actual e na nova medicao
+     */
     public void estimateRTT(){
-        long newRTT=(1-alpha)*estimatedRTT+alpha*sampleRTT;
+        long newRTT=(long) ((1-alpha)*estimatedRTT+alpha*sampleRTT);
         estimatedRTT=newRTT;
     }
     
+    /*
+     * Calcula desvio padrão do RTT
+     */
+    public void calculateDevRTT(){
+        long newdevRTT=(long) ((1-beta)*devRTT+beta*(sampleRTT-estimatedRTT));
+        devRTT=newdevRTT;
+    }
     
+    public void calculateTimeOut(){
+        timeout=estimatedRTT+4*devRTT;
+    }
+    
+    
+    @Override
     public void run(){
-        
+        Long current, total;
+        while(keeprunning){
+            for(long time : timeCountList){
+                if(time!=0){
+                    current=System.currentTimeMillis();
+                    total=current-time;
+                    if(total>timeout)
+                        MainCliente.getSender().setTamanhoJanelaInicial(
+                                MainCliente.getSender().getTamanhoJanela()/2);
+                }
+            }
+        }
     }
 }
